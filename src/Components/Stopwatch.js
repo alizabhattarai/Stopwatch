fb4a376547634f88c4a4062dc3b1878827432643
alizabhattarai <alizabi2000@gmail.com>
2020-07-30 21:50:08 -0500

    Third commit

---
+++ src/Components/Stopwatch.js
@@ -0,0 +1,33 @@
+// first create a component called stopwatch 
+// a class component was used to define the component
+// setting timerOn=false a boolean value, timerStart and timertime  to zero '0' value
+// startTimer, stopTimer and resetTimer is used with this.setState method 
+// math.floor method is used to calulate the with centiseconds, seconds, minuts and hours
+// onClick start timer, stop timer and Rest timer are used with this.state in return 
+
+
+
+
+
+
+
+import React from 'react';
+class Stopwatch extends React.Component {
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Stopwatch;
